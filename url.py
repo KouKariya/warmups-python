6742acfb94b801720376ae836aac4760ee72fe15
KouKariya <KouKariya@users.noreply.github.com>
2016-07-17 10:50:16 -0500

    Update url.py
    Added link to original article containing this code.

--- url.py
+++ url.py
@@ -1,3 +1,6 @@
+#url.py by Jesse Gallarzo
+#Original source for code can be found at: https://www.hackthissite.org/articles/read/1053/
+
 import urllib2
 url = "http://www.smogon.com" #Random site to test code
 website = urllib2.urlopen(url)
