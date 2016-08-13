2a264ccdf1dcca0a91b8e0efe048d7025bae2689
KouKariya <KouKariya@users.noreply.github.com>
2016-08-12 23:26:34 -0500

    Update url.py

--- url.py
+++ url.py
@@ -1,5 +1,6 @@
 #url.py by Jesse Gallarzo
 #Original source for code can be found at: https://www.hackthissite.org/articles/read/1053/
+#TEST
 
 import urllib2
 url = "http://www.smogon.com" #Random site to test code
