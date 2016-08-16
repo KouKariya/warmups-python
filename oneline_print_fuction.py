1a9a0af0d9c982a4a69a0e4f967419603ddc0760
KouKariya <jssgallarzo@gmail.com>
2016-08-16 13:38:49 -0500

    Added oneline_print_function script to work on. Cleaned up comments in url.py

---
+++ oneline_print_fuction.py
@@ -0,0 +1,6 @@
+#oneline_print_function.py by Jesse Gallarzo
+#Following code uses Python 2.7
+#Objective: print a range of numbers without spaces or symbols all in one line
+
+#TODO find a way to strip brackets and commas without using regular expressions
+print(range(1, int(input())+1))
