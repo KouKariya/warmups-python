c0c621cb7a518d43f722de487f8248087a91bec9
KouKariya <jssgallarzo@gmail.com>
2016-09-01 08:35:55 -0500

    Added product.py

---
+++ product.py
@@ -0,0 +1,13 @@
+#product.py
+#Written by Jesse Gallarzo
+#Written in Python3
+ans = 1
+arr=[]
+N = int(input())
+for num in N:
+	arr.append(int(input())
+#TODO fix invalid sytax on bottom line
+for val in N:
+	ans = (ans*arr[val])%(10**9 + 7)
+	
+print ans
