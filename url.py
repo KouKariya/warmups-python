47880a57f358064deaaa58aa722d1be16ab6e2df
KouKariya <jssgallarzo@gmail.com>
2016-11-16 19:12:13 -0600

    Test push

--- url.py
+++ url.py
@@ -1,6 +1,7 @@
 #url.py by Jesse Gallarzo
 #Original source for code can be found at: https://www.hackthissite.org/articles/read/1053/
 #Following code uses Python 2.7
+#TEST
 
 import urllib2
 url = "http://www.smogon.com" #Random site to test code
