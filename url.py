f94f96504895ecf25b1607859be8415e4d65c901
KouKariya <jssgallarzo@gmail.com>
2016-11-19 08:55:38 -0600

    Updated url.py to accept any website as its 1st argument to run script on.

--- url.py
+++ url.py
@@ -1,9 +1,10 @@
 #url.py by Jesse Gallarzo
 #Original source for code can be found at: https://www.hackthissite.org/articles/read/1053/
 #Following code uses Python 2.7
-#TEST
 
 import urllib2
-url = "http://www.smogon.com" #Random site to test code
+import sys
+
+url = str(sys.argv[1]) #1st arg is used as the website to run the script on
 website = urllib2.urlopen(url)
 print website.read() #lookup .read()
